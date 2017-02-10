b7c271c04f9e6fa4729387adefdd039a867dcdb2
Rui Gil <ruipgil@gmail.com>
2017-02-10 19:24:31 +0000

    Store stories on cache with mongodb

--- shinarnews/Story.py
+++ shinarnews/Story.py
@@ -1,16 +1,48 @@
 class Story:
-    def __init__(self, title, link, author, lang="en", translations=None):
+    def __init__(self, id, title, link, author, lang="en", translations=None):
+        self.id = id
         self.title = title
         self.link = link
         self.author = author
         self.lang = lang
+
         if translations is None:
             self.translations = []
         else:
             self.translations = translations
 
+    def save(self, db):
+        if db.stories.find_one({'id': self.id}) is None:
+            db.stories.insert_one(self.to_json())
+
+    def translate(self, target_lang):
+        return
+
+    def to_json(self):
+        return {
+                'id': self.id,
+                'title': self.title,
+                'link': self.link,
+                'author': self.author,
+                'lang': self.lang,
+                'translations': [trans.to_json() for trans in self.translations]
+                }
+
     @staticmethod
     def from_json(json):
         if json['type'] == 'story':
-            return Story(json['title'], json['url'], json['by'])
+            return Story(json['id'], json['title'], json['url'], json['by'])
         return None
+
+    @staticmethod
+    def from_db(story):
+        translations = [Story.from_db(trans) for trans in story['translations']]
+
+        return Story(
+            story['id'],
+            story['title'],
+            story['link'],
+            story['author'],
+            story['lang'],
+            translations
+        )
--- shinarnews/shinarnews.py
+++ shinarnews/shinarnews.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
+from Story import Story
 from hn import top_stories, get_item_details
+from pymongo import MongoClient
 
 STORIES_PER_PAGE = 10
 
@@ -8,14 +10,31 @@ app = Flask(__name__)
 # app.config.from_object(__name__)
 # app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
 
+client = MongoClient('localhost', 32768)
+mongo = client.test
+
+def get_stories():
+    stories_ids = top_stories(STORIES_PER_PAGE)
+
+    _stories = []
+    for story_id in stories_ids:
+        m_stories = mongo.stories
+        cached_story = m_stories.find_one({'id': story_id})
+        if cached_story is None:
+            story = get_item_details(story_id)
+            story.save(mongo)
+        else:
+            story = Story.from_db(cached_story)
+        _stories.append(story)
+
+    return _stories
 
 @app.route('/')
 def stories():
     """ Shows the stories on HackerNews
     """
-    stories = [get_item_details(story) for story in top_stories(STORIES_PER_PAGE)]
 
-    return render_template('top.html', stories=stories)
+    return render_template('top.html', stories=get_stories())
 
 
 if __name__ == '__main__':
